ebbc2665b16e503517290b84e097c8737cad1dfc
Ilya Pashkov <ispashkov@gmail.com>
2020-09-01 08:52:57 +0400

    Initial Commit

---
+++ futopedia-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  futopedia-ios
+//
+//  Created by Ilya Pashkov on 9/1/20.
+//  Copyright © 2020 Ilya Pashkov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
